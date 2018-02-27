ace18f18d690d021a5a269dbe13a0ce81543793c
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-27 05:37:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,5 @@
 
+
 class Transfer
   # your code here
   attr_accessor :sender, :receiver, :amount, :status
@@ -16,14 +17,14 @@ class Transfer
 
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
-        @sender.balance -= @amount
-        @receiver.balance += @amount
-        @status = "complete"
-      else
-        @status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-      end
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
+  end
 
   def reverse_transfer
     if @status == "complete"
