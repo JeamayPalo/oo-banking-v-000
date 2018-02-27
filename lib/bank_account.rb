c393d5d12d2b69a51c908bb0a4d1cd8b086fa8b5
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-27 04:07:43 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,18 @@ class BankAccount
     "Your balance is $#{@balance}."
   end
 
-  def valid
+  def valid?
+    
   end
+
+  describe '#valid?' do
+    it "is valid with an open status and a balance greater than 0" do
+      @broke = BankAccount.new("Kat Dennings")
+      @broke.balance = 0
+      @closed = BankAccount.new("Beth Behrs")
+      @closed.status = "closed"
+      expect(avi.valid?).to eq(true)
+      expect(@broke.valid?).to eq(false)
+      expect(@closed.valid?).to eq(false)
 
 end
